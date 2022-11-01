ed4aa059444633df9b20e808c71da08bc2337154
Marlon Wesley de Paula <marlonestudanteadm31@hotmail.com>
2022-11-01 17:39:43 -0300

    Subtraindo o produto e a soma de um número inteiro

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class DIO {
+        
+static void Main(string[] args){
+
+        int numeroEntrada = int.Parse(Console.ReadLine());
+        int produto = 1, soma = 0;
+        while (numeroEntrada > 0){
+             int l = numeroEntrada % 10;
+
+             produto *= l;
+             soma += l;
+             numeroEntrada /= 10;
+           }
+
+           Console.WriteLine(produto-soma);
+        }
+    }
